b14ec48244fcff26d980b5fcacb21a0e345bb5eb
Ubuntu <ubuntu@ip-172-31-21-139.ap-northeast-1.compute.internal>
2021-06-29 14:00:22 +0000

    Add SearchBox

---
+++ src/components/Navbar.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <b-navbar type="dark" variant="dark">
+        <b-navbar-brand tag="h1" id="brand" class="px-4">Vue Music</b-navbar-brand>
+        <b-navbar-nav>
+          <!--検索-->
+          <Search id="search"></Search>
+        </b-navbar-nav>
+      </b-navbar>
+  </div>
+</template>
+
+<script>
+import Search from '@/components/Search.vue'
+
+export default {
+  name: 'Navbar',
+  components:{
+    Search
+  }
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+
+#brand {
+  color: #01ffc3;
+}
+
+#search{
+  margin-left:auto;
+}
+</style>
---
+++ src/components/Search.vue
@@ -0,0 +1,61 @@
+<template>
+  <b-input-group size="sm" id="search">
+    <b-input-group-prepend is-text>
+      <b-icon icon="search"></b-icon>
+    </b-input-group-prepend>
+    
+    <b-form-input type="search" placeholder="Search music" id="searchbox" @change="SearchMusic($event)"></b-form-input>
+  </b-input-group>
+</template>
+
+<script>
+
+export default {
+  name: 'Search',
+  
+  data(){
+    return{
+      clientID:'07e27cb2ce6f456d80d64df6cacfbeef',
+      clienetSecret:'cc911f3f4ac2486794f2fcf816b51d16'
+    }
+  },
+  
+  methods:{
+    SearchMusic: async function(text){
+      const accessToken = 'Bearer ' + await this.GetAccessToken();
+      const response = await fetch("https://api.spotify.com/v1/search?q="+text+"&type=album,track&limit=5", {
+        headers: {
+          Authorization: accessToken,
+        },
+        method: "GET"
+      });
+      
+      const obj = await response.json();
+      console.log(obj);
+    },
+
+    //it gets AccessToken to connect API    
+    GetAccessToken: async function(){
+      const rawClientCredential = this.clientID + ':' + this.clienetSecret;
+      const clientCredential = "Basic " + btoa(rawClientCredential);
+      const response = await fetch("https://accounts.spotify.com/api/token", {
+        body: "grant_type=client_credentials",
+        headers: {
+          Authorization: clientCredential,
+          "Content-Type": "application/x-www-form-urlencoded"
+        },
+        method: "POST"
+      });
+      const obj = await response.json();
+      
+      return obj.access_token;
+    }
+  }
+}
+</script>
+
+<style>
+#searchbox{
+  padding-right: 250px;
+}
+</style>
